e68124847c35766af39cf9e62c0fd3b4a6a2ed68
spektr001 <sanek777.28.03.2000@gmail.com>
2022-11-15 05:44:35 +0200

    footer + start page ready

---
+++ src/components/StartPage/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { AppBar } from "@mui/material";
+import { Box } from "@mui/material";
+import { Toolbar } from "@mui/material";
+import { Typography } from "@mui/material";
+
+export const Footer = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+            WebPocketDoc
+          </Typography>
+          <span>by <a target="_blank" rel="noreferrer" href="https://github.com/spektr001">spectrum001</a></span> 
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
