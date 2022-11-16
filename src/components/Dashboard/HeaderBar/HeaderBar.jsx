84267deec2e47dabbe46b60345b927d179b4f752
spektr001 <sanek777.28.03.2000@gmail.com>
2022-11-16 03:05:53 +0200

    headerbar menu

--- src/components/Dashboard/HeaderBar/HeaderBar.jsx
+++ src/components/Dashboard/HeaderBar/HeaderBar.jsx
@@ -1,45 +1,79 @@
 import * as React from "react";
+import { useNavigate } from 'react-router-dom'
 import { AppBar } from "@mui/material";
 import { Box } from "@mui/material";
 import { Toolbar } from "@mui/material";
 import { Typography } from "@mui/material";
 import { IconButton } from "@mui/material";
+import { Menu } from "@mui/material";
+import { MenuItem } from "@mui/material";
 import crossIcon from "../../../assets/images/cross.png";
 import heartIcon from "../../../assets/images/heart.png";
 
 export const HeaderBar = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const navigate = useNavigate()
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const logout = () => {
+    setAnchorEl(null);
+    navigate('/')
+  }
+
   return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        marginBottom: 15,
-      }}
-    >
-      <AppBar position="fixed">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="primary"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <img width="50px" src={crossIcon} alt="redcross" />
-          </IconButton>
-          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-            WebPocketDoc
-          </Typography>
-          <IconButton
-            size="large"
-            edge="start"
-            color="primary"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <img width="40px" src={heartIcon} alt="heart" />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <>
+      <Box
+        sx={{
+          flexGrow: 1,
+          marginBottom: 15,
+        }}
+      >
+        <AppBar position="fixed">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="primary"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <img width="50px" src={crossIcon} alt="redcross" />
+            </IconButton>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+              WebPocketDoc
+            </Typography>
+            <IconButton
+              onClick={handleClick}
+              size="large"
+              edge="start"
+              color="primary"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <img width="40px" src={heartIcon} alt="heart" />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={logout}>Logout</MenuItem>
+      </Menu>
+    </>
   );
 };
